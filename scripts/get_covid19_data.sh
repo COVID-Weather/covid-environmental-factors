2d29e5f0d53ec16500966a563808dfc690a23f5b
hdrake <henrifdrake@gmail.com>
2020-03-27 16:51:00 -0400

    Added provincial epidemiological data
    For South Korea, China, Italy (and more?)

---
+++ scripts/get_covid19_data.sh
@@ -0,0 +1,10 @@
+cd ../data
+# Country-level globally and Province/State-level for China and US
+git clone https://github.com/CSSEGISandData/COVID-19.git
+
+# Province/State-level for Korea
+git clone https://github.com/parksw3/COVID19-Korea.git
+
+# Province/State-level & County-level for Italy and China
+git clone https://github.com/bolliger32/gpl-covid
+cd ../scripts
