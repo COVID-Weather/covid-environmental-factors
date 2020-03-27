7cbd6d79b58f4527561dafddbed8ef10929bbbbc
Gregory Wagner <wagner.greg@gmail.com>
2020-03-27 11:21:19 -0400

    More small improvements to ERA5 download script

--- scripts/download_era5_temperature_dewpoint_pressure.py
+++ scripts/download_era5_temperature_dewpoint_pressure.py
@@ -1,25 +1,37 @@
-import cdsapi
+import cdsapi, os
 
 c = cdsapi.Client()
 
-filepath = '../data/thermodynamic_variables'
+# ERA5 reanalysis data will be downloaded into ../data
+datadir = os.path.join('..', 'data')
+prefix = 'thermodynamic_variables'
+pathprefix = os.path.join(datadir, prefix)
 
+# Download temperature, pressure, and dewpoint temperature to permit humidity calculation
 thermodynamic_variables = ['2m_dewpoint_temperature', '2m_temperature', 'surface_pressure']
 
-every_two_hours = ["{:02d}:00".format(i) for i in range(0, 24, 2)]
-every_hour = ["{:02d}:00".format(i) for i in range(24)]
+# Download data every day in the specified months
 every_day = ["{:02d}".format(i) for i in range(32)]
 
-# Sent requests via the CDS API to download data for 2019 and 2020
+# Download hourly data in the specified days
+every_hour = ["{:02d}:00".format(i) for i in range(24)]
+every_two_hours = ["{:02d}:00".format(i) for i in range(0, 24, 2)]
+frequency = every_hour # change this to download less-frequent data
+
+# Send requests via the CDS API to download data for 2019 and 2020.
+#
 # Setup instructions here: https://cds.climate.copernicus.eu/api-how-to
+#
+# Global data is downloaded for 11/2019 to 03/2020.
+
 c.retrieve('reanalysis-era5-single-levels', 
            {'product_type': 'reanalysis',
                   'format': 'netcdf',
                 'variable': thermodynamic_variables,
                     'year': '2019',
                    'month': ['11'],
-                    'time': every_hour,
-                     'day': every_day}, '{}_11_2019.nc'.format(filepath))
+                    'time': frequency,
+                     'day': every_day}, '{}_11_2019.nc'.format(pathprefix))
 
 c.retrieve('reanalysis-era5-single-levels', 
            {'product_type': 'reanalysis',
@@ -27,8 +39,8 @@ c.retrieve('reanalysis-era5-single-levels',
                 'variable': thermodynamic_variables,
                     'year': '2019',
                    'month': ['12'],
-                    'time': every_hour,
-                     'day': every_day}, '{}_12_2019.nc'.format(filepath))
+                    'time': frequency,
+                     'day': every_day}, '{}_12_2019.nc'.format(pathprefix))
 
 c.retrieve('reanalysis-era5-single-levels', 
            {'product_type': 'reanalysis',
@@ -36,8 +48,8 @@ c.retrieve('reanalysis-era5-single-levels',
                 'variable': thermodynamic_variables,
                     'year': '2020',
                    'month': ['01'],
-                    'time': every_hour,
-                     'day': every_day}, '{}_01_2020.nc'.format(filepath))
+                    'time': frequency,
+                     'day': every_day}, '{}_01_2020.nc'.format(pathprefix))
 
 c.retrieve('reanalysis-era5-single-levels', 
            {'product_type': 'reanalysis',
@@ -45,8 +57,8 @@ c.retrieve('reanalysis-era5-single-levels',
                 'variable': thermodynamic_variables,
                     'year': '2020',
                    'month': ['02'],
-                    'time': every_hour,
-                     'day': every_day}, '{}_02_2020.nc'.format(filepath))
+                    'time': frequency,
+                     'day': every_day}, '{}_02_2020.nc'.format(pathprefix))
 
 c.retrieve('reanalysis-era5-single-levels', 
            {'product_type': 'reanalysis',
@@ -54,5 +66,5 @@ c.retrieve('reanalysis-era5-single-levels',
                 'variable': thermodynamic_variables,
                     'year': '2020',
                    'month': ['03'],
-                    'time': every_hour,
-                     'day': every_day}, '{}_03_2020.nc'.format(filepath))
+                    'time': frequency,
+                     'day': every_day}, '{}_03_2020.nc'.format(pathprefix))
